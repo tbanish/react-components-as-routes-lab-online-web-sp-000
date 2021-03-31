48ca97514d1dc407ef28888dbfd19f2e409b90de
tbanish <tombanish@gmail.com>
2021-03-31 00:12:19 -0400

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch
 } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import Home from '../components/Home';
@@ -13,7 +14,27 @@ import Movies from '../components/Movies';
 const App = (props) => {
   return (
     <Router>
-      {/*{code here}*/}
+      {
+        <div className="navbar">
+            <Route>
+              <NavBar/>
+            </Route>
+            <Switch>
+              <Route exact path="/">
+                <Home/>
+              </Route>
+              <Route path="/movies">
+                <Movies/>
+              </Route>
+              <Route path="/directors">
+                <Directors/>
+              </Route>
+              <Route path="/actors">
+                <Actors/>
+              </Route>
+            </Switch>
+        </div>
+      }
     </Router>
   );
 };
