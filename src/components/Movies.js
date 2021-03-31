eaa4be679a1d53ecc6ba079dd63932d2ac3b2225
tbanish <tombanish@gmail.com>
2021-03-31 00:11:51 -0400

    commit tree 12131e87fc7846405dc9902cf5f97593a9072e9b

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  console.log(movies)
   return (
     <div>
-        {/*{code here}*/}
+
+      <h1>Movies Page</h1>
+
+      {movies.map(movie =>
+        <div className="movie">{movie.title}: {movie.time}min
+        <ul>
+          {movie.genres.map(genre =>
+            <li>{genre}</li>
+          )}
+        </ul>
+        </div>
+      )}
+
     </div>
   );
 };
